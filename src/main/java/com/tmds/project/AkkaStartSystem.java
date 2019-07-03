58811899321ef5b8db84d9a610c629bd0c25ee4d
Andrea Tupini <tupini07@gmail.com>
2019-07-03 17:11:19 +0200

    some basic structure and initial setup

---
+++ src/main/java/com/tmds/project/AkkaStartSystem.java
@@ -0,0 +1,120 @@
+package com.tmds.project;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class AkkaStartSystem {
+    public static void main(String[] args) {
+        final ActorSystem system = ActorSystem.create("DMX");
+
+        try {
+            final ActorRef n1 = system.actorOf(NodeAct.props(1), "node_1");
+            final ActorRef n2 = system.actorOf(NodeAct.props(2), "node_2");
+            final ActorRef n3 = system.actorOf(NodeAct.props(3), "node_3");
+            final ActorRef n4 = system.actorOf(NodeAct.props(4), "node_4");
+            final ActorRef n5 = system.actorOf(NodeAct.props(5), "node_5");
+            final ActorRef n6 = system.actorOf(NodeAct.props(6), "node_6");
+            final ActorRef n7 = system.actorOf(NodeAct.props(7), "node_7");
+            final ActorRef n8 = system.actorOf(NodeAct.props(8), "node_8");
+//            final ActorRef n9 = system.actorOf(NodeAct.props(9), "node_9");
+//            final ActorRef n10 = system.actorOf(NodeAct.props(10), "node_10");
+//            final ActorRef n11 = system.actorOf(NodeAct.props(11), "node_11");
+//            final ActorRef n12 = system.actorOf(NodeAct.props(12), "node_12");
+//            final ActorRef n13 = system.actorOf(NodeAct.props(13), "node_13");
+//            final ActorRef n14 = system.actorOf(NodeAct.props(14), "node_14");
+//            final ActorRef n15 = system.actorOf(NodeAct.props(15), "node_15");
+//            final ActorRef n16 = system.actorOf(NodeAct.props(16), "node_16");
+//            final ActorRef n17 = system.actorOf(NodeAct.props(17), "node_17");
+//            final ActorRef n18 = system.actorOf(NodeAct.props(18), "node_18");
+
+            n1.tell(new NodeAct.SetNeighbors(new HashSet<ActorRef>(Arrays.asList(
+                    n2
+            ))), ActorRef.noSender());
+
+
+            n2.tell(new NodeAct.SetNeighbors(new HashSet<ActorRef>(Arrays.asList(
+                    n1, n3
+            ))), ActorRef.noSender());
+
+
+            n3.tell(new NodeAct.SetNeighbors(new HashSet<ActorRef>(Arrays.asList(
+                    n2
+            ))), ActorRef.noSender());
+
+
+            n4.tell(new NodeAct.SetNeighbors(new HashSet<ActorRef>(Arrays.asList(
+                    n2, n6
+            ))), ActorRef.noSender());
+
+
+            n5.tell(new NodeAct.SetNeighbors(new HashSet<ActorRef>(Arrays.asList(
+                    n6
+            ))), ActorRef.noSender());
+
+
+            n6.tell(new NodeAct.SetNeighbors(new HashSet<ActorRef>(Arrays.asList(
+                    n4, n7, n5, n8
+            ))), ActorRef.noSender());
+
+
+            n7.tell(new NodeAct.SetNeighbors(new HashSet<ActorRef>(Arrays.asList(
+                    n6
+            ))), ActorRef.noSender());
+
+
+            n8.tell(new NodeAct.SetNeighbors(new HashSet<ActorRef>(Arrays.asList(
+                    n6
+            ))), ActorRef.noSender());
+
+
+            // choose a random node as the initial possessor of the token
+            n4.tell(new NodeAct.Initialize(true), ActorRef.noSender());
+
+            System.out.println(">>> Press ENTER to exit <<<");
+            System.in.read();
+        } catch (IOException ioe) {
+        } finally {
+            system.terminate();
+        }
+
+        // ------
+        // below is the old try for stating the "quickstart" system
+
+//    try {
+//      //#create-actors
+//      final ActorRef printerActor =
+//        system.actorOf(Printer.props(), "printerActor");
+//      final ActorRef howdyGreeter =
+//        system.actorOf(Greeter.props("Howdy", printerActor), "howdyGreeter");
+//      final ActorRef helloGreeter =
+//        system.actorOf(Greeter.props("Hello", printerActor), "helloGreeter");
+//      final ActorRef goodDayGreeter =
+//        system.actorOf(Greeter.props("Good day", printerActor), "goodDayGreeter");
+//      //#create-actors
+//
+//      //#main-send-messages
+//      howdyGreeter.tell(new Greeter.WhoToGreet("Akka"), ActorRef.noSender());
+//      howdyGreeter.tell(new Greeter.Greet(), ActorRef.noSender());
+//
+//      howdyGreeter.tell(new Greeter.WhoToGreet("Lightbend"), ActorRef.noSender());
+//      howdyGreeter.tell(new Greeter.Greet(), ActorRef.noSender());
+//
+//      helloGreeter.tell(new Greeter.WhoToGreet("Java"), ActorRef.noSender());
+//      helloGreeter.tell(new Greeter.Greet(), ActorRef.noSender());
+//
+//      goodDayGreeter.tell(new Greeter.WhoToGreet("Play"), ActorRef.noSender());
+//      goodDayGreeter.tell(new Greeter.Greet(), ActorRef.noSender());
+//      //#main-send-messages
+//
+//      System.out.println(">>> Press ENTER to exit <<<");
+//      System.in.read();
+//    } catch (IOException ioe) {
+//    } finally {
+//      system.terminate();
+//    }
+    }
+}
---
+++ src/main/java/com/tmds/project/NodeAct.java
@@ -0,0 +1,92 @@
+package com.tmds.project;
+
+import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.actor.Props;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+
+public class NodeAct extends AbstractActor {
+
+    private LoggingAdapter log = Logging.getLogger(getContext().getSystem(), this);
+
+    // Private variables that identify this node
+    private final int node_id; // for debugging
+    private HashSet<ActorRef> neighbors;
+
+    // Variables used to implement the algorithm
+    private ActorRef holder; // reference to self or to one of the neighbors
+    private boolean using; // whether this node is using the token (in the CS)
+    private LinkedList<ActorRef> request_q; // FIFO queue holding the requests for the token that this node is processing
+    private boolean asked; // whether this node has asked a neighbor for the node
+
+
+    public NodeAct(int node_id) {
+        this.node_id = node_id;
+        this.request_q = new LinkedList<ActorRef>();
+    }
+
+
+    static public Props props(int node_id) {
+        return Props.create(NodeAct.class, () -> new NodeAct(node_id));
+    }
+
+    // ----------------------------------------------------
+    // Message classes that are handled
+    static public class Initialize {
+        public final boolean is_first; // whether this is the first node in the flood or not
+
+        public Initialize(boolean is_first) {
+            this.is_first = is_first;
+        }
+    }
+
+    static public class SetNeighbors {
+        public final HashSet<ActorRef> neighbors;
+
+        public SetNeighbors(HashSet<ActorRef> neighbors) {
+            this.neighbors = neighbors;
+        }
+    }
+
+    // ----------------------------------------------------
+
+    // implementation of handling for messages
+    private void setNeighbors(SetNeighbors msg) {
+        log.info("Setting neighbors. Size: {}", msg.neighbors.size());
+        this.neighbors = msg.neighbors;
+    }
+
+    private void handleInitialize(Initialize msg) {
+        if (this.holder != null) {
+            // if this node has already recieved the initialize message then don't
+            // propagate it further
+            return;
+        }
+
+        log.info("Initializing node: {}", this.node_id);
+
+        if (msg.is_first) {
+            this.holder = getSender();
+        } else {
+            this.holder = getSelf();
+        }
+
+        for (ActorRef neighbor : this.neighbors) {
+            neighbor.tell(new Initialize(false), getSelf());
+        }
+    }
+
+    // ----------------------------------------------------
+    // mapping between message classes and methods for handling
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+                .match(Initialize.class, this::handleInitialize)
+                .match(SetNeighbors.class, this::setNeighbors)
+                .build();
+    }
+}
